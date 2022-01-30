56dd80e8c442dc6f02c4dfae12ffd471db6d7dcf
Sander Seiersnes <Sander.seiersnes@gmail.com>
2022-01-30 18:53:03 +0100

    noice

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.getElementById("make-profile").onclick = function() {
+    var boks = document.getElementById("boks");
+    var make_profile = document.getElementById("make-profile");
+
+    var login = document.getElementById("login");
+
+    boks.style.display = "block"
+    make_profile.style.display = "none"
+    login.style.display = "none"
+    input.style.display = "block"
+    
+}
